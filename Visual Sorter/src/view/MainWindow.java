5dfcb5fbb4f5e5763a1417cf647f4aeadaad1c0c
Justin B <JustinB98@users.noreply.github.com>
2018-09-08 01:11:55 -0400

    Removed a print line
    Added that line to test the syncing. Removed it now that this project is configured.

--- Visual Sorter/src/view/MainWindow.java
+++ Visual Sorter/src/view/MainWindow.java
@@ -40,7 +40,6 @@ public class MainWindow extends Application {
 
 	@Override
 	public void init() throws Exception {
-		System.out.println(Thread.currentThread().getName());
 		// order of method calls are for a reason
 		// initCheckBox();
 		// initRadioButtons();
